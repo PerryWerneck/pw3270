61c32a7ae84ef0d20204cafaf028486bbd576f6b
perry.werneck@gmail.com <perry.werneck@gmail.com@d25448e0-c022-0410-b998-a785f7053de0>
2012-03-28 12:50:37 +0000

    Atualizando identificacao de revisao/versao

---
+++ bootstrap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if test -z $1; then
+	out=`dirname $0`
+else
+	out="$1"
+fi
+
+if test -e revision ; then
+	. revision
+fi
+
+PACKAGE_REVISION=`date +%y%m%d`
+
+if test -d ".svn" ; then
+
+	SVN=`which svn 2> /dev/null`
+
+	if test -x "$SVN" ; then
+
+		TEMPFILE=$(mktemp)
+		LANG="EN_US"
+		$SVN info > $TEMPFILE 2> /dev/null
+		PACKAGE_REVISION=$(cat $TEMPFILE | grep "^Revision: " | cut -d" " -f2)
+		PACKAGE_SOURCE=$(cat $TEMPFILE | grep "^URL: " | cut -d" " -f2)
+
+		rm -f $TEMPFILE
+	fi
+
+
+fi
+
+echo "PACKAGE_REVISION=$PACKAGE_REVISION" > $out/revision
+echo "PACKAGE_SOURCE=$PACKAGE_SOURCE" >> $out/revision
+
+echo "m4_define([SVN_REVISION], $PACKAGE_REVISION)" > $out/revision.m4
+echo "m4_define([SVN_URL], $PACKAGE_SOURCE)" >> $out/revision.m4
+
