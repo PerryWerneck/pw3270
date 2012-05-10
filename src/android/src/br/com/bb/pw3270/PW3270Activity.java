e1bd8880249320cb216969b1f4a3cf004ef8f93e
perry.werneck@gmail.com <perry.werneck@gmail.com@d25448e0-c022-0410-b998-a785f7053de0>
2012-05-10 13:46:57 +0000

    Testando criação de módulo jni para android

---
+++ src/android/src/br/com/bb/pw3270/PW3270Activity.java
@@ -0,0 +1,13 @@
+package br.com.bb.pw3270;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PW3270Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ src/android/src/br/com/bb/pw3270/lib3270.java
@@ -0,0 +1,21 @@
+package br.com.bb.pw3270;
+
+public class lib3270 {
+
+   static 
+   {
+	   System.loadLibrary("3270jni");
+   }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+	
+	 public native String getVersion();
+
+}
