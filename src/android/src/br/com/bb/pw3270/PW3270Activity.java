64a583570d8ca5b51a119f79265c40e7df00ce5a
perry.werneck@gmail.com <perry.werneck@gmail.com@d25448e0-c022-0410-b998-a785f7053de0>
2012-05-14 11:33:55 +0000

    Testando acesso à lib3270 via jni

--- src/android/src/br/com/bb/pw3270/PW3270Activity.java
+++ src/android/src/br/com/bb/pw3270/PW3270Activity.java
@@ -2,12 +2,22 @@ package br.com.bb.pw3270;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PW3270Activity extends Activity {
+	
+	lib3270 host;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        TextView text = (TextView) findViewById(R.id.text);
+        
+        host = new lib3270();
+        
+        text.setText(host.getVersion());
     }
 }
