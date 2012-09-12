7b076f4e38b718b050ad2dec4b30097f4c2a2f92
perry.werneck@gmail.com <perry.werneck@gmail.com@d25448e0-c022-0410-b998-a785f7053de0>
2012-09-12 20:38:07 +0000

    Geração dos .deb no build service apresentou problemas depois das ultimas alterações

--- makedeb.sh
+++ makedeb.sh
@@ -5,7 +5,13 @@ VERSION=$(grep AC_INIT configure.ac | cut -d[ -f3 | cut -d] -f1)
 TIMESTAMP=$(LANG=en_US date)
 . ./revision
 
-rm -f debian/changelog
+rm -fr debian
+mkdir debian
+
+echo 7 > debian/compat
+cp debian.control debian/control
+cp debian.rules debian/control
+
 EDITOR=true dch --preserve -v $VERSION-$PACKAGE_LEVEL -u low --create --package pw3270
 sed -i "s@UNRELEASED@unstable@;s@Initial release. (Closes: #XXXXXX)@SVN Revision $PACKAGE_REVISION@" debian/changelog
 
