17264499996be0cec2e338372555623993b1d662
perry.werneck@gmail.com <perry.werneck@gmail.com@d25448e0-c022-0410-b998-a785f7053de0>
2013-07-17 12:46:10 +0000

    Ajustando detecção do runtime gtk

--- src/include/pw3270/class.h
+++ src/include/pw3270/class.h
@@ -18,7 +18,7 @@
  * programa;  se  não, escreva para a Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA, 02111-1307, USA
  *
- * Este programa está nomeado como pw3270class.c e possui - linhas de código.
+ * Este programa está nomeado como class.h e possui - linhas de código.
  *
  * Contatos:
  *
