0c585ebf979ae18fd21e51fe312b2a6fc73ee220
perry.werneck@gmail.com <perry.werneck@gmail.com@d25448e0-c022-0410-b998-a785f7053de0>
2014-03-28 12:05:34 +0000

    Remodelando interface libreoffice

---
+++ src/oxt/research.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+OO_SDK_HOME=/usr/lib64/libreoffice/sdk
+IDLC=/usr/lib64/libreoffice/sdk/bin/idlc
+CPPUMAKER=/usr/lib64/libreoffice/sdk/bin/cppumaker
+TYPES_RDB=/usr/lib64/libreoffice/ure/share/misc/types.rdb
+REGMERGE=/usr/lib64/libreoffice/ure/bin/regmerge
+
+$IDLC -C -I$OO_SDK_HOME/idl -O. pw3270.idl
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+$REGMERGE pw3270.rdb /UCR pw3270.urd
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+
+$CPPUMAKER -O./include -Tpw3270.lib3270 $TYPES_RDB pw3270.rdb
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+echo ok
+
