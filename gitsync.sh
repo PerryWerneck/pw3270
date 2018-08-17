fe240cba90ffcccaca997ed92bca5edf4c09d064
Perry Werneck <perry.werneck@gmail.com>
2018-08-16 22:47:42 -0300

    Incluindo script para sincronização com o bitbucket.

---
+++ gitsync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# https://help.github.com/articles/syncing-a-fork/
+#
+# https://help.github.com/articles/configuring-a-remote-for-a-fork/
+
+git fetch origin
+git checkout master
+git merge origin/master
+
+
