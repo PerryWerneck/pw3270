930299ab9a924d9f148d332469a3d9656beae6bd
Perry Werneck <perry.werneck@gmail.com>
2018-11-09 13:19:56 -0200

    Incluindo sincronização com github.

--- gitsync.sh
+++ gitsync.sh
@@ -8,10 +8,13 @@
 #
 # git remote add bitbucket https://bitbucket.org/pw3270/pw3270-application.git
 #
+# git remote add github https://github.com/PerryWerneck/pw3270.git
+# git push github --all
+#
 
 git fetch origin
 git checkout master
 git merge origin/master
 
-git push bitbucket
+git push github
 
