a0fce292bdf07b14afd04a2dedb20b5d0bb63665
Perry Werneck <perry.werneck@gmail.com>
2019-02-26 09:44:45 -0300

    Updating autogen

--- autogen.sh
+++ autogen.sh
@@ -1,47 +1,12 @@
 #!/bin/bash
 
-APPLEVEL="0"
-
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
+olddir=`pwd`
 cd "$srcdir"
 
-if test -e revision ; then
-	. revision
-fi
-
-touch ChangeLog
-
-# Obtém revisão via git
-# Referência: http://stackoverflow.com/questions/4120001/what-is-the-git-equivalent-for-revision-number
-
-# Obtém URL via git
-PACKAGE_SOURCE=$(git config --get remote.origin.url)
-
-# Obtém número total de commits
-PACKAGE_REVISION=$(git rev-list HEAD --count)
-
-if test -z $PACKAGE_REVISION ; then
-	echo "Can´t detect package revision, using current date"
-	PACKAGE_REVISION=`date +%y%m%d`
-fi
-
-if test -z $PACKAGE_SOURCE ; then
-	echo "Can´t detect package source, using default one"
-	PACKAGE_SOURCE="https://portal.softwarepublico.gov.br/social/pw3270/"
-fi
-
-echo "PACKAGE_REVISION=$PACKAGE_REVISION" > $srcdir/revision
-echo "PACKAGE_SOURCE=$PACKAGE_SOURCE" >> $srcdir/revision
-echo "PACKAGE_LEVEL=$APPLEVEL" >> $srcdir/revision
-
-echo "m4_define([SVN_REVISION], $PACKAGE_REVISION)" > $srcdir/revision.m4
-echo "m4_define([SVN_URL], $PACKAGE_SOURCE)" >> $srcdir/revision.m4
-echo "m4_define([APP_LEVEL], $APPLEVEL)" >> $srcdir/revision.m4
-
-mkdir -p scripts
-automake --add-missing 2> /dev/null | true
+mkdir -p m4
 
 aclocal
 if test $? != 0 ; then
@@ -55,8 +20,12 @@ if test $? != 0 ; then
 	exit -1
 fi
 
-echo "Package set to revision $PACKAGE_REVISION and source $PACKAGE_SOURCE"
+mkdir -p scripts
+automake --add-missing 2> /dev/null | true
 
 cd "$olddir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
+
+
 
