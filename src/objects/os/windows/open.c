b0d210f734029fff6afb40dfe6ae9941a91afcce
Perry Werneck <perry.werneck@gmail.com>
2021-08-26 17:17:52 -0300

    Fixing windows startup with session file name.

---
+++ src/objects/os/windows/open.c
@@ -0,0 +1,207 @@
+/*
+ * "Software pw3270, desenvolvido com base nos códigos fontes do WC3270  e X3270
+ * (Paul Mattes Paul.Mattes@usa.net), de emulação de terminal 3270 para acesso a
+ * aplicativos mainframe. Registro no INPI sob o nome G3270.
+ *
+ * Copyright (C) <2008> <Banco do Brasil S.A.>
+ *
+ * Este programa é software livre. Você pode redistribuí-lo e/ou modificá-lo sob
+ * os termos da GPL v.2 - Licença Pública Geral  GNU,  conforme  publicado  pela
+ * Free Software Foundation.
+ *
+ * Este programa é distribuído na expectativa de  ser  útil,  mas  SEM  QUALQUER
+ * GARANTIA; sem mesmo a garantia implícita de COMERCIALIZAÇÃO ou  de  ADEQUAÇÃO
+ * A QUALQUER PROPÓSITO EM PARTICULAR. Consulte a Licença Pública Geral GNU para
+ * obter mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto com este
+ * programa; se não, escreva para a Free Software Foundation, Inc., 51 Franklin
+ * St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Este programa está nomeado como - e possui - linhas de código.
+ *
+ * Contatos:
+ *
+ * perry.werneck@gmail.com	(Alexandre Perry de Souza Werneck)
+ * erico.mendonca@gmail.com	(Erico Mascarenhas Mendonça)
+ *
+ */
+
+#include <config.h>
+
+#include <pw3270.h>
+#include <lib3270.h>
+#include <lib3270/log.h>
+#include <pw3270/application.h>
+#include <pw3270/window.h>
+#include <v3270.h>
+#include <v3270/keyfile.h>
+#include <v3270/dialogs.h>
+
+/*
+static gchar * v3270_keyfile_find(const gchar *name) {
+	//
+	// It can be a session file, scans for it
+	//
+	g_autofree gchar * appdir = g_win32_get_package_installation_directory_of_module(NULL);
+
+	const gchar * paths[] = {
+		g_get_user_special_dir(G_USER_DIRECTORY_DOCUMENTS),
+		g_get_user_config_dir(),
+		appdir
+	};
+
+	static const gchar *subdirs[] = {
+		"3270",
+		G_STRINGIFY(PRODUCT_NAME),
+		PACKAGE_NAME
+	};
+
+	size_t path, subdir;
+
+	g_autofree gchar * filename = g_strconcat(name,".3270",NULL);
+
+	for(path = 0; path < G_N_ELEMENTS(paths); path++) {
+
+		// Try subdirs.
+		for(subdir = 0; subdir < G_N_ELEMENTS(subdirs); subdir++) {
+
+			gchar * fullpath = g_build_filename(paths[path],subdirs[subdir],filename,NULL);
+
+			debug("Searching for \"%s\"",fullpath);
+
+			if(g_file_test(fullpath,G_FILE_TEST_IS_REGULAR)) {
+				return fullpath;
+			}
+			g_free(fullpath);
+
+		}
+
+		// Try path.
+		{
+			gchar * fullpath = g_build_filename(paths[path],filename,NULL);
+
+			debug("Searching for \"%s\"",fullpath);
+
+			if(g_file_test(fullpath,G_FILE_TEST_IS_REGULAR)) {
+				return fullpath;
+			}
+			g_free(fullpath);
+		}
+
+	}
+
+	return NULL;
+
+}
+*/
+
+/// @brief Open session file
+static void open(GtkApplication *application, GtkWindow **window, const gchar *filename) {
+
+	g_message("Opening '%s'",filename);
+
+	if(*window) {
+
+		// Already open a window, open in new tab.
+		pw3270_application_window_new_tab(GTK_WIDGET(*window), filename);
+
+	} else {
+		// It's a new window
+		*window = GTK_WINDOW(pw3270_application_window_new(application, filename));
+
+	}
+
+}
+
+void pw3270_application_open_file(GtkApplication *application, GtkWindow **window, GFile *file) {
+
+	g_autofree gchar *filename = g_file_get_path(file);
+
+	if(g_file_query_exists(file,NULL) && filename) {
+
+		// The file exists, load it.
+		open(application,window,filename);
+
+	} else {
+
+		GtkWidget * dialog = gtk_message_dialog_new_with_markup(
+								 NULL,
+								 0,
+								 GTK_MESSAGE_ERROR,
+								 GTK_BUTTONS_CLOSE,
+								 _("Can't load session preferences")
+							 );
+
+		if(filename) {
+			gtk_message_dialog_format_secondary_text(GTK_MESSAGE_DIALOG(dialog),_("Unable to load session preferences from '%s'"),filename);
+		}
+
+		gtk_window_set_title(GTK_WINDOW(dialog),_("Error starting session"));
+
+		gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER);
+		gtk_widget_show_all(dialog);
+
+		//g_signal_connect(dialog,"close",G_CALLBACK(gtk_widget_destroy),NULL);
+		//g_signal_connect(dialog,"response",G_CALLBACK(gtk_widget_destroy),NULL);
+		gtk_dialog_run(GTK_DIALOG(dialog));
+
+		g_application_quit(G_APPLICATION(application));
+
+	}
+
+
+	/*
+	g_autofree gchar * scheme = g_file_get_uri_scheme(file);
+
+	if(g_ascii_strcasecmp(scheme,"file") == 0) {
+
+		// It's a file scheme.
+		if(g_file_query_exists(file,NULL)) {
+
+			// The file exists, load it.
+			g_autofree gchar *filename = g_file_get_path(file);
+			open(application,window,filename);
+
+		} else {
+
+			// Search for file.
+			g_autofree gchar * basename = g_file_get_basename(file);
+			g_autofree gchar * filename = v3270_keyfile_find(basename);
+
+			if(filename) {
+				open(application,window,filename);
+			} else {
+				g_warning("Cant find session '%s'",basename);
+			}
+
+		}
+
+	} else if(g_ascii_strcasecmp(scheme,"tn3270") == 0 || g_ascii_strcasecmp(scheme,"tn3270s") == 0) {
+
+		g_autofree gchar * uri = g_file_get_uri(file);
+		size_t sz = strlen(uri);
+
+		if(sz > 0 && uri[sz-1] == '/')
+			uri[sz-1] = 0;
+
+		g_message("Opening '%s' with default settings",uri);
+
+		if(!*window) {
+			*window = GTK_WINDOW(pw3270_application_window_new(application, NULL));
+		} else {
+			pw3270_application_window_new_tab(GTK_WIDGET(*window), NULL);
+		}
+
+		GtkWidget * terminal = pw3270_application_window_get_active_terminal(GTK_WIDGET(*window));
+		v3270_set_default_session(terminal);
+		v3270_set_url(terminal,uri);
+
+	} else {
+
+		g_warning("Don't know how to handle '%s' scheme",scheme);
+
+	}
+	*/
+
+}
