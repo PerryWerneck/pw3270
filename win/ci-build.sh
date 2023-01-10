6c9cc6f79cea723c0707fee5525e7946d9dcc8de
Perry Werneck <perry.werneck@gmail.com>
2023-01-10 17:04:03 -0300

    Implementing ci-build script.

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -18,34 +18,48 @@ die ( ) {
 
 cd $(dirname $(dirname $(readlink -f ${0})))
 
+rm -fr .build
+
 #
 # Build LIB3270
 #
 echo "Building lib3270"
-rm -fr ./wmi
-git clone https://github.com/PerryWerneck/lib3270.git ./lib3270 > $LOGFILE 2>&1 || die "clone lib3270 failure"
-pushd lib3270
+mkdir -p .build/lib3270
+git clone https://github.com/PerryWerneck/lib3270.git ./.build/lib3270 > $LOGFILE 2>&1 || die "clone lib3270 failure"
+pushd ./.build/lib3270
 ./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
 ./configure > $LOGFILE 2>&1 || die "Configure failure"
 make clean > $LOGFILE 2>&1 || die "Make clean failure"
 make all  > $LOGFILE 2>&1 || die "Make failure"
-popd ..
+popd
 
-export LIB3270_CFLAGS="-I./lib3270/src/include"
-export LIB3270_LIBS="-L./lib3270/.bin/Release -l3270.delayed"
+export LIB3270_CFLAGS="-I./.build/lib3270/src/include"
+export LIB3270_LIBS="-L./.build/lib3270/.bin/Release -l3270.delayed"
 
 #
 # Build LIBV3270
 #
+echo "Building libv3270"
+mkdir -p .build/libv3270
+git clone https://github.com/PerryWerneck/libv3270.git ./.build/libv3270 > $LOGFILE 2>&1 || die "clone libv3270 failure"
+pushd ./.build/libv3270
+./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
+./configure > $LOGFILE 2>&1 || die "Configure failure"
+make clean > $LOGFILE 2>&1 || die "Make clean failure"
+make all  > $LOGFILE 2>&1 || die "Make failure"
+popd
+
+export LIBV3270_CFLAGS="-I./.build/libv3270/src/include"
+export LIBV3270_LIBS="-L./.build/libv3270/.bin/Release -lv3270"
 
 #
 # Build PW3270
 #
-#echo "Building VMDETECT"
-#./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
-#./configure > $LOGFILE 2>&1 || die "Configure failure"
-#make clean > $LOGFILE 2>&1 || die "Make clean failure"
-#make all  > $LOGFILE 2>&1 || die "Make failure"
+echo "Building PW3270"
+./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
+./configure > $LOGFILE 2>&1 || die "Configure failure"
+make clean > $LOGFILE 2>&1 || die "Make clean failure"
+make all  > $LOGFILE 2>&1 || die "Make failure"
 
 echo "Build complete"
 
