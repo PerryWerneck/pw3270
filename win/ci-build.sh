18a5942319f603e618a503f18c7e07eadfdd8c94
Perry Werneck <perry.werneck@gmail.com>
2023-01-11 03:30:29 -0300

    Trying to fix mingw ci-build

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -16,46 +16,55 @@ die ( ) {
 	exit -1
 }
 
-cd $(dirname $(dirname $(readlink -f ${0})))
+myDIR=$(dirname $(dirname $(readlink -f ${0})))
+echo "myDIR=${myDIR}"
 
-rm -fr .build
+cd ${myDIR}
+rm -fr ${myDIR}/.build
 
 #
 # Build LIB3270
 #
 echo "Building lib3270"
-mkdir -p .build/lib3270
-git clone https://github.com/PerryWerneck/lib3270.git ./.build/lib3270 > $LOGFILE 2>&1 || die "clone lib3270 failure"
-pushd ./.build/lib3270
+mkdir -p ${myDIR}/.build/lib3270
+git clone https://github.com/PerryWerneck/lib3270.git ${myDIR}/.build/lib3270 > $LOGFILE 2>&1 || die "clone lib3270 failure"
+pushd  ${myDIR}/.build/lib3270
 ./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
 ./configure > $LOGFILE 2>&1 || die "Configure failure"
 make clean > $LOGFILE 2>&1 || die "Make clean failure"
 make all  > $LOGFILE 2>&1 || die "Make failure"
 popd
 
-export LIB3270_CFLAGS="-I./.build/lib3270/src/include"
-export LIB3270_LIBS="-L./.build/lib3270/.bin/Release -l3270.delayed"
+export LIB3270_CFLAGS="-I${myDIR}/.build/lib3270/src/include"
+export LIB3270_LIBS="-L${myDIR}/.build/lib3270/.bin/Release -l3270.delayed"
+
+echo "LIB3270_CFLAGS=${LIB3270_CFLAGS}"
+echo "LIB3270_LIBS=${LIB3270_LIBS}"
 
 #
 # Build LIBV3270
 #
 echo "Building libv3270"
-mkdir -p .build/libv3270
-git clone https://github.com/PerryWerneck/libv3270.git ./.build/libv3270 > $LOGFILE 2>&1 || die "clone libv3270 failure"
-pushd ./.build/libv3270
+mkdir -p  ${myDIR}/.build/libv3270
+git clone https://github.com/PerryWerneck/libv3270.git ${myDIR}/.build/libv3270 > $LOGFILE 2>&1 || die "clone libv3270 failure"
+pushd ${myDIR}/.build/libv3270
 ./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
 ./configure > $LOGFILE 2>&1 || die "Configure failure"
 make clean > $LOGFILE 2>&1 || die "Make clean failure"
 make all  > $LOGFILE 2>&1 || die "Make failure"
 popd
 
-export LIBV3270_CFLAGS="-I./.build/libv3270/src/include ${LIB3270_CFLAGS}"
-export LIBV3270_LIBS="-L./.build/libv3270/.bin/Release -lv3270 ${LIB3270_LIBS}"
+export LIBV3270_CFLAGS="-I${myDIR}/.build/libv3270/src/include ${LIB3270_CFLAGS}"
+export LIBV3270_LIBS="-L${myDIR}/.build/libv3270/.bin/Release -lv3270 ${LIB3270_LIBS}"
+
+echo "LIBV3270_CFLAGS=${LIBV3270_CFLAGS}"
+echo "LIBV3270_LIBS=${LIBV3270_LIBS}"
 
 #
 # Build PW3270
 #
 echo "Building PW3270"
+cd ${myDIR}
 ./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
 ./configure > $LOGFILE 2>&1 || die "Configure failure"
 make clean > $LOGFILE 2>&1 || die "Make clean failure"
