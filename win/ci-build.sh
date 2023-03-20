99ead83500500bc66a34ea551ea88758562531f2
Perry Werneck <perry.werneck@gmail.com>
2023-03-20 14:16:53 -0300

    Implementing winpkg workflow.

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -25,30 +25,43 @@ rm -fr ${myDIR}/.build
 #
 # Build LIB3270
 #
-echo "Building lib3270"
-mkdir -p ${myDIR}/.build/lib3270
-git clone https://github.com/PerryWerneck/lib3270.git ${myDIR}/.build/lib3270 > $LOGFILE 2>&1 || die "clone lib3270 failure"
-pushd  ${myDIR}/.build/lib3270
-./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
-./configure > $LOGFILE 2>&1 || die "Configure failure"
-make clean > $LOGFILE 2>&1 || die "Make clean failure"
-make all  > $LOGFILE 2>&1 || die "Make failure"
-make install  > $LOGFILE 2>&1 || die "Install failure"
-popd
+if [ -e mingw-lib3270.tar.xz ]; then
+
+	echo "Unpacking lib3270"
+	tar -C / -Jxvf mingw-lib3270.tar.xz 
+
+else
+	echo "Building lib3270"
+	git clone https://github.com/PerryWerneck/lib3270.git ./.build/lib3270 || die "clone lib3270 failure"
+	cd ./.build/lib3270
+	./autogen.sh || die "Lib3270 autogen failure"
+	./configure || die "Lib3270 Configure failure"
+	make clean || die "Lib3270 Make clean failure"
+	make all || die "Lib3270 Make failure"
+	make install || die "Lib3270 Install failure"
+	cd ../..
+fi
 
 #
 # Build LIBV3270
 #
-echo "Building libv3270"
-mkdir -p  ${myDIR}/.build/libv3270
-git clone https://github.com/PerryWerneck/libv3270.git ${myDIR}/.build/libv3270 > $LOGFILE 2>&1 || die "clone libv3270 failure"
-pushd ${myDIR}/.build/libv3270
-./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
-./configure > $LOGFILE 2>&1 || die "Configure failure"
-make clean > $LOGFILE 2>&1 || die "Make clean failure"
-make all  > $LOGFILE 2>&1 || die "Make failure"
-make install  > $LOGFILE 2>&1 || die "Install failure"
-popd
+if [ -e mingw-libv3270.tar.xz ]; then
+
+	echo "Unpacking libv3270"
+	tar -C / -Jxvf mingw-lib3270.tar.xz 
+
+else
+	echo "Building libv3270"
+	mkdir -p  ${myDIR}/.build/libv3270
+	git clone https://github.com/PerryWerneck/libv3270.git ${myDIR}/.build/libv3270 > $LOGFILE 2>&1 || die "clone libv3270 failure"
+	pushd ${myDIR}/.build/libv3270
+	./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
+	./configure > $LOGFILE 2>&1 || die "Configure failure"
+	make clean > $LOGFILE 2>&1 || die "Make clean failure"
+	make all  > $LOGFILE 2>&1 || die "Make failure"
+	make install  > $LOGFILE 2>&1 || die "Install failure"
+	popd
+fi
 
 #
 # Build PW3270
@@ -59,6 +72,9 @@ cd ${myDIR}
 ./configure > $LOGFILE 2>&1 || die "Configure failure"
 make clean > $LOGFILE 2>&1 || die "Make clean failure"
 make all  > $LOGFILE 2>&1 || die "Make failure"
+
+make DESTDIR=.bin/package install
+tar --create --xz --file=mingw-pw3270.tar.xz --directory=.bin/package --verbose .
 
 echo "Build complete"
 
